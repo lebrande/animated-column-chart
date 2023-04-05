b0f2611a74888444c684f1024c9b0e1894e01836
JS Dzem <jsdzem@gmail.com>
2023-04-05 23:14:54 +0200

    🏄‍♂️ Chart surface

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -8,16 +8,23 @@ export const Chart = () => {
 };
 
 const TopSurface = () => {
+  const sizeClassNames = 'h-16 w-[4.7rem]';
+  const transformClassNames = 'skew-x-[-30deg] rotate-[30deg] -mb-3.5';
+  const gradientClassNames = 'bg-gradient-to-br from-indigo-950 via-blue-950 to-blue-800';
+
   return (
-    <div className="h-16 w-[4.7rem] bg-white skew-x-[-30deg] rotate-[30deg] -mb-3.5" />
+    <div className={`${sizeClassNames} ${transformClassNames} ${gradientClassNames}`} />
   );
 }
 
 const FrontSurface = () => {
+  const sizeClassNames = 'h-56 w-16';
+  const gradientClassNames = 'bg-gradient-to-b from-indigo-950 to-blue-800';
+
   return (
     <div className="flex">
-      <div className="h-56 w-16 bg-red-500 skew-y-[30deg]" />
-      <div className="h-56 w-16 bg-blue-500 skew-y-[-30deg]" />
+      <div className={`${sizeClassNames} skew-y-[30deg] ${gradientClassNames}`} />
+      <div className={`${sizeClassNames} skew-y-[-30deg] ${gradientClassNames}`} />
     </div>
   );
 }
